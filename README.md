7944d00f31e729faf538c7fa9272ef42cfbdf8c9
Jesse Dinkin <kaeon.ace@gmail.com>
2020-12-26 16:31:00 -0500

    Kaeon

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1 align="center">United C</h1>
+<h2 align="center">A Modernized C/C++ Hybrid Language</h2>
---
+++ United C/Source/UCC.js
@@ -0,0 +1,40 @@
+let makefile =
+`CC      = gcc
+CFLAGS  = -g
+RM      = rm -f
+
+default: all
+
+all: [APP_NAME]
+
+[APP_NAME]: [APP_NAME].c
+    $(CC) $(CFLAGS) -o [APP_NAME] [APP_NAME].c
+
+clean veryclean:
+    $(RM) [APP_NAME]`;
+
+try {
+
+	var cmd = require("node-cmd");
+	var io = require("./io.js");
+	var oneSuite = require("./ONESuite.js");
+
+	let path = process.argv[2];
+
+	if(path.indexOf(".") == -1)
+		path += ".uc";
+
+	let appName =
+		path.substring(0, path.lastIndexOf(".")).split(" ").join("_");
+
+	let code = oneSuite.preprocess(io.open(path));
+
+	io.save(code, appName + ".c");
+	io.save(makefile.split("[APP_NAME]").join(appName), "Makefile");
+
+	cmd.run("make");
+}
+
+catch(error) {
+	console.log(error);
+}
