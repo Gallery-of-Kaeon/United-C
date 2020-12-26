f8e12677b38591e0c181a66c077f6bac673e3c8d
Jesse Dinkin <kaeon.ace@gmail.com>
2020-12-26 16:58:58 -0500

    Kaeon

---
+++ United C/Shadow Host Bootstrap/UCC.js
@@ -0,0 +1,40 @@
+let makefile =
+`CC      = gcc
+CFLAGS  = -g
+RM      = rm -f
+
+default: all
+
+all: [APP_NAME]
+
+[APP_NAME]: [APP_NAME].c
+    $(CC) $(CFLAGS) -o [APP_NAME] [APP_NAME].c
+
+clean veryclean:
+    $(RM) [APP_NAME]`;
+
+try {
+
+	var cmd = require("node-cmd");
+	var io = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/JavaScript-Utilities/master/JavaScript%20Utilities/IO/ioNode.js");
+	var oneSuite = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/JavaScript-Utilities/master/JavaScript%20Utilities/United%20Bootstrap/ONESuite.js");
+
+	let path = process.argv[2];
+
+	if(path.indexOf(".") == -1)
+		path += ".uc";
+
+	let appName =
+		path.substring(0, path.lastIndexOf(".")).split(" ").join("_");
+
+	let code = oneSuite.preprocess(io.open(path));
+
+	io.save(code, appName + ".c");
+	io.save(makefile.split("[APP_NAME]").join(appName), "Makefile");
+
+	cmd.run("make");
+}
+
+catch(error) {
+	console.log(error);
+}
